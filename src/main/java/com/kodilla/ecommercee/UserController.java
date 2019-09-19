7069e9e554fc7e38c5fc0ec43a24a0447b2c1882
Tomasz Mentel <mentell@o2.pl>
2019-09-19 20:26:21 +0200

    User endpoints,package domain with (User,UserDto) class.

--- src/main/java/com/kodilla/ecommercee/UserController.java
+++ src/main/java/com/kodilla/ecommercee/UserController.java
@@ -1,4 +1,26 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domain.UserDto;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("v1/user")
 public class UserController {
+    @RequestMapping(method = RequestMethod.POST, value = "createUser")
+    public void createUser(@RequestBody UserDto userDto) {
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getUserKey")
+    public UserDto getUserKey(@RequestParam Long userKey) {
+        return new UserDto();
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteUserKey")
+    public void deleteUserKey(@RequestParam Long userKey) {
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "UpdateUser")
+    public UserDto updateUser(@RequestBody UserDto userDto) {
+        return new UserDto();
+    }
 }
