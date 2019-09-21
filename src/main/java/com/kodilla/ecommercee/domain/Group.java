9c86be0185a51ad567f53efe970245efc2866607
Tomasz Mentel <mentell@o2.pl>
2019-09-21 17:06:02 +0200

    Encja Group - implementacja

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -1,4 +1,59 @@
 package com.kodilla.ecommercee.domain;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table
 public class Group {
+    private String name;
+    private long id;
+    private List<Product> products = new ArrayList<>();
+
+    public Group() {
+    }
+
+    public Group(String name, long id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    @Column(name = "NAME")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @OneToMany(
+            targetEntity = Product.class,
+            mappedBy = "group",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
 }
--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -1,4 +1,32 @@
 package com.kodilla.ecommercee.domain;
 
+import javax.persistence.*;
+
+@Entity
+@Table
 public class Product {
+
+    private long id;
+    private Group group;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "GROUP_ID")
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
 }
