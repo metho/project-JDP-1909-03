dcc1209157bdba52136bd5fba10ed337c7a8454d
przskraj <pskrajnyy@gmail.com>
2019-09-22 14:57:38 +0200

    Encja User - implementacja

--- src/main/java/com/kodilla/ecommercee/domain/Order.java
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -1,4 +1,35 @@
 package com.kodilla.ecommercee.domain;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
 public class Order {
+
+    private long id;
+    private User user;
+
+    public Order() {
+    }
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ID", unique = true)
+    public long getId() {
+        return id;
+    }
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    public User getUser() {
+        return user;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -1,4 +1,94 @@
 package com.kodilla.ecommercee.domain;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Entity
 public class User {
+    private long id;
+    private String name;
+    private String surname;
+    private Address address;
+    private List<Order> orders;
+    private Cart cart;
+
+    public User() {
+    }
+
+    public User(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ID", unique = true)
+    public long getId() {
+        return id;
+    }
+
+    @NotNull
+    @Column(name = "NAME")
+    public String getName() {
+        return name;
+    }
+
+    @NotNull
+    @Column(name = "SURNAME")
+    public String getSurname() {
+        return surname;
+    }
+
+    @OneToOne(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+    @JoinColumn(name = "ADDRESS_ID")
+    public Address getAddress() {
+        return address;
+    }
+
+    @OneToMany(
+            targetEntity = Order.class,
+            mappedBy = "user",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    @OneToOne(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+    @JoinColumn(name = "CART_ID")
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
 }
