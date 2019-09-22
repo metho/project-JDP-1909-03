e56a302eabeccfab4c7495eab0d60ceff44b04a5
przskraj <pskrajnyy@gmail.com>
2019-09-22 14:57:38 +0200

    Encja User - implementacja

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -1,4 +1,33 @@
 package com.kodilla.ecommercee.domain;
 
+import javax.persistence.*;
+
 public class Cart {
+
+    private long id;
+    private User user;
+
+    public Cart() {
+    }
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ID", unique = true)
+    public long getId() {
+        return id;
+    }
+
+    @OneToOne
+    @JoinColumn(name = "USER_ID")
+    public User getUser() {
+        return user;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
