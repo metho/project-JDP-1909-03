49770f4838b2afcf18d62aebcd0cc4e81d601b37
matimarkiewicz <mati.markiewicz@gmail.com>
2019-09-22 23:12:58 +0200

    JDP190903-4: create endpoints in OrderController

--- src/main/java/com/kodilla/ecommercee/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/OrderController.java
@@ -1,4 +1,40 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domain.OrderDto;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping("/v1/orders")
 public class OrderController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/getOrders")
+    public List<OrderDto> getOrders() {
+        System.out.println("List of orders");
+        return new ArrayList<>();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/getOrderById")
+    public OrderDto getOrder(@RequestParam Long orderId) {
+        System.out.println("List of orders by id");
+        return new OrderDto();
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/createOrder", consumes = APPLICATION_JSON_VALUE)
+    public void createOrder(@RequestBody OrderDto orderDto) {
+        System.out.println("Create new order.");
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/updateOrder")
+    public OrderDto updateOrders(@RequestBody OrderDto orderDto) {
+        System.out.println("Update the order");
+        return new OrderDto();
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/deleteOrder")
+    public void deleteOrder(@RequestParam Long orderId) {
+        System.out.println("Delete order by id");
+    }
 }
