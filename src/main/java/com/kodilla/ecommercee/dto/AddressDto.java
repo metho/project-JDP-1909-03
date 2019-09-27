8c8f1e5a6aa433788eea827b6b1957049f7c7c66
KhrystynaLialiuk <50339148+KhrystynaLialiuk@users.noreply.github.com>
2019-09-27 20:40:16 +0200

    JDP190903-33: Dto objects implementation (#15)
    * JDP190903-33: Dto objects implementation
    
    * JDP190903-33: DTO fixed
    
    * JDP190903-33: resolving conflicts
    
    * JDP190903-33: fixing conflicts with controller subpackage

--- src/main/java/com/kodilla/ecommercee/dto/AddressDto.java
+++ src/main/java/com/kodilla/ecommercee/dto/AddressDto.java
@@ -1,4 +1,16 @@
 package com.kodilla.ecommercee.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddressDto {
+    private long id;
+    private String street;
+    private String code;
+    private String city;
+    private long userId;
 }
--- src/main/java/com/kodilla/ecommercee/dto/UserOrderDto.java
+++ src/main/java/com/kodilla/ecommercee/dto/UserOrderDto.java
@@ -1,4 +1,19 @@
 package com.kodilla.ecommercee.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserOrderDto {
+    private long id;
+    private String number;
+    private LocalDate orderDate;
+    private long userId;
+    private List<ProductDto> products;
 }
