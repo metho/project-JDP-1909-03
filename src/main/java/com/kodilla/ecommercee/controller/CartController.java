df23386441877a78cf236f17cc1fc5a9f4cf8497
meggmaier <megg.maier@gmail.com>
2019-09-29 08:49:16 +0200

    CartController prepared for testing

--- src/main/java/com/kodilla/ecommercee/controller/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -3,6 +3,8 @@ package com.kodilla.ecommercee.controller;
 import com.kodilla.ecommercee.dto.CartDto;
 import com.kodilla.ecommercee.dto.UserOrderDto;
 import com.kodilla.ecommercee.dto.ProductDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,29 +14,38 @@ import java.util.List;
 public class CartController {
 
     static final String BASE_API = "v1/cart";
+    private static final Logger LOGGER = LoggerFactory.getLogger(CartController.class);
 
     @PostMapping("new")
     public void createEmptyCart() {
+        LOGGER.info("Creating an empty cart.");
         //implementation
     }
 
     @GetMapping("products")
     public List<ProductDto> getCartProducts() {
+        LOGGER.info("Getting cart products.");
+
         return new ArrayList<>();
     }
 
     @PutMapping("product")
     public CartDto addProductToCart(ProductDto productDto) {
+        LOGGER.info("Adding product to cart");
+
         return new CartDto();
     }
 
     @DeleteMapping("product")
     public CartDto deleteProductFromCart(Long productId) {
+        LOGGER.info("Deleting product with id: " + productId + " from cart");
+
         return new CartDto();
     }
 
     @PostMapping("order")
     public UserOrderDto createOrderForCart(Long cartId) {
+        LOGGER.info("Creating order for cart nr: " + cartId);
         return new UserOrderDto();
     }
 }
